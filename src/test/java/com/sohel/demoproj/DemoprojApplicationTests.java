275785708db5b69a2fa91321adea9d2076acff35
Sohel Datta <soheldatta17@gmail.com>
2026-08-16 10:46:23 +0530

    first commit

---
+++ src/test/java/com/sohel/demoproj/DemoprojApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sohel.demoproj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoprojApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
