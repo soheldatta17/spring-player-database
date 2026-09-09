2ee62c015de182c7cc89e2209292e0fc6c9d9956
Sohel Datta <soheldatta17@gmail.com>
2026-09-09 14:19:25 +0530

    Update dependencies and configuration for Spring Boot 3.4.3, add Swagger API documentation support, and disable open-in-view warning

--- src/main/java/com/sohel/demoproj/DemoprojApplication.java
+++ src/main/java/com/sohel/demoproj/DemoprojApplication.java
@@ -2,8 +2,12 @@ package com.sohel.demoproj;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@EnableMongoRepositories(basePackages = "com.sohel.demoproj.repository")
+@EnableJpaRepositories(basePackages = "com.sohel.demoproj.jpa")
 public class DemoprojApplication {
 
 	public static void main(String[] args) {
@@ -11,3 +15,5 @@ public class DemoprojApplication {
 	}
 
 }
+
+
---
+++ src/main/java/com/sohel/demoproj/config/SwaggerConfig.java
@@ -0,0 +1,19 @@
+package com.sohel.demoproj.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Demo API")
+                        .version("1.0")
+                        .description("Demo Project API Documentation"));
+    }
+}
